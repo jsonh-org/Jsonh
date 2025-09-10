8742c76719bcbac081ad3eb3b1a35b2b39de61d3
Joyless <65855333+Joy-less@users.noreply.github.com>
2025-09-10 02:27:14 +0100

    Implement site

---
+++ site/src/main.ts
@@ -0,0 +1,26 @@
+import { JsonhReader } from "jsonh-ts";
+
+const input = document.getElementById('input') as HTMLTextAreaElement;
+const output = document.getElementById('output') as HTMLTextAreaElement;
+const prettyPrint = document.getElementById('pretty-print') as HTMLInputElement;
+
+function convert(): void {
+    if (input.value.trim().length === 0) {
+        output.value = "";
+        return;
+    }
+
+    let elementResult = JsonhReader.parseElementFromString(input.value);
+    if (elementResult.isError) {
+        output.value = `Error: ${elementResult.error.message}`;
+        return;
+    }
+
+    let indent: string = prettyPrint.checked ? "    " : "";
+
+    output.value = JSON.stringify(elementResult.value, null, indent);
+}
+
+input.addEventListener('input', convert);
+prettyPrint.addEventListener('input', convert);
+convert();
