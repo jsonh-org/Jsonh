723fb0649917b4e8d35acafe8d7569c2cad50753
Joyless <65855333+Joy-less@users.noreply.github.com>
2026-05-14 21:47:27 +0100

    Add copy URL functionality to site

--- site/src/main.ts
+++ site/src/main.ts
@@ -1,9 +1,45 @@
 import { JsonhReader, JsonhReaderOptions } from "jsonh-ts";
+import { stringToBase64URL, stringFromBase64URL } from "./base64url";
 
 const input = document.getElementById('input') as HTMLTextAreaElement;
 const output = document.getElementById('output') as HTMLTextAreaElement;
 const prettyPrint = document.getElementById('pretty-print') as HTMLInputElement;
 const jsonhVersion = document.getElementById('jsonh-version') as HTMLSelectElement;
+const shareButton = document.getElementById('share-button') as HTMLButtonElement;
+
+function loadPrefill(): void {
+    const params = new URLSearchParams(window.location.search);
+    const sharedInput: string | null = params.get("jsonh");
+
+    if (sharedInput !== null) {
+        input.value = stringFromBase64URL(sharedInput);
+    }
+}
+
+loadPrefill();
+
+async function copyPrefillToClipboard(): Promise<void> {
+    const url = new URL(window.location.href);
+    url.searchParams.set("jsonh", stringToBase64URL(input.value));
+
+    window.history.replaceState({}, "", url);
+
+    try {
+        await navigator.clipboard.writeText(url.toString());
+
+        const temporaryShareButtonText: string = "Copied!";
+
+        if (shareButton.textContent !== temporaryShareButtonText) {
+            const originalShareButtonText: string = shareButton.textContent;
+
+            shareButton.textContent = temporaryShareButtonText;
+            setTimeout(() => shareButton.textContent = originalShareButtonText, 1_000);
+        }
+    }
+    catch { }
+}
+
+shareButton.addEventListener('click', copyPrefillToClipboard);
 
 function convert(): void {
     if (input.value.trim().length === 0) {
@@ -11,7 +47,7 @@ function convert(): void {
         return;
     }
 
-    let elementResult = JsonhReader.parseElementFromString(input.value, new JsonhReaderOptions({
+    const elementResult = JsonhReader.parseElementFromString(input.value, new JsonhReaderOptions({
         parseSingleElement: true,
         version: parseInt(jsonhVersion.value),
     }));
@@ -20,7 +56,7 @@ function convert(): void {
         return;
     }
 
-    let indent: string = prettyPrint.checked ? "    " : "";
+    const indent: string = prettyPrint.checked ? "    " : "";
 
     output.value = JSON.stringify(elementResult.value, null, indent);
 }
